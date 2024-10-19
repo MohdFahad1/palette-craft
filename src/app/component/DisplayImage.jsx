b5dc76ac677b32b69481f3f681b1cc08451fb25b
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2024-10-19 11:34:32 +0530

    feat: Extracted and displayed color palettes

--- src/app/component/DisplayImage.jsx
+++ src/app/component/DisplayImage.jsx
@@ -6,13 +6,26 @@ function DisplayImage({ image }) {
   const [palettes, setPalettes] = useState([]);
   const [loaded, setLoaded] = useState(false);
 
+  console.log(palettes);
+
+  const rgbToHex = (r, g, b) =>
+    "#" +
+    [r, g, b]
+      .map((x) => {
+        const hex = x.toString(16);
+        return hex.length === 1 ? "0" + hex : hex;
+      })
+      .join("");
+
   useEffect(() => {
     if (loaded && imageRef.current) {
       const colorThief = new ColorThief();
       try {
         const palette = colorThief.getPalette(imageRef.current, 5);
-        setPalettes(palette);
-        console.log(palette);
+        const hexPalette = palette.map((color) =>
+          rgbToHex(color[0], color[1], color[2])
+        );
+        setPalettes(hexPalette);
       } catch (err) {
         console.error("Error extracting colors:", err);
       }
@@ -20,7 +33,7 @@ function DisplayImage({ image }) {
   }, [image, loaded]);
 
   return (
-    <main className="flex items-center justify-center p-5">
+    <main className="flex flex-col items-center justify-center p-5">
       <div className="relative h-[400px] w-[500px]">
         {!loaded && (
           <div className="flex flex-col items-center justify-center text-center">
@@ -42,6 +55,24 @@ function DisplayImage({ image }) {
           onLoad={() => setLoaded(true)}
           style={{ width: "100%", height: "100%", objectFit: "contain" }}
         />
+      </div>
+      <div className="flex gap-10 mt-5">
+        {palettes.map((hex, index) => (
+          <div
+            key={index}
+            style={{
+              backgroundColor: hex,
+            }}
+            className="flex justify-between flex-col items-center h-[150px] w-[180px] rounded-xl cursor-pointer"
+          >
+            <div className="flex items-center justify-center h-full">
+              <h1>{hex}</h1>
+            </div>
+            <div className="flex items-center justify-center w-full h-8 border-t-2 border-white">
+              Click to Copy
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   );
